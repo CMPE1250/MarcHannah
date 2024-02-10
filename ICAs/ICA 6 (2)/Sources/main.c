a48168df7f7905450e19d22c97a407fedbcff7db
MarcHannah-NAIT <149616632+MarcHannah-NAIT@users.noreply.github.com>
2024-02-09 20:18:55 -0700

    Add files via upload

--- ICAs/ICA 6 (2)/Sources/main.c
+++ ICAs/ICA 6 (2)/Sources/main.c
@@ -35,7 +35,7 @@
 /********************************************************************/
 // Local Prototypes
 /********************************************************************/
-
+int GetCount(void);
 /********************************************************************/
 // Global Variables
 /********************************************************************/
@@ -66,92 +66,133 @@ void main(void)
 
   for (;;)
   {
-   /* if (SWL_Pushed(SWL_LEFT) > 0)
-    {
-      SWL_ON(SWL_RED);
-    }
-    else if (SWL_Pushed(SWL_LEFT) == 0)
-    {
-      SWL_OFF(SWL_RED);
-    }
-
-    if (SWL_Pushed(SWL_RIGHT) > 0)
-    {
-      SWL_ON(SWL_GREEN);
-    }
-    else if (SWL_Pushed(SWL_RIGHT) == 0)
-    {
-      SWL_OFF(SWL_GREEN);
-    }
-
-    if (SWL_Pushed(SWL_CTR) > 0)
-    {
-      SWL_ON(SWL_YELLOW);
-    }
-    else if (SWL_Pushed(SWL_CTR) == 0)
-    {
-      SWL_OFF(SWL_YELLOW);
-    } */
-  
-
-
-
-
- if (SWL_Pushed(SWL_LEFT)>0 & GetCount()<=2){SWL_TOG(SWL_RED);}
-
-if (SWL_Pushed(SWL_RIGHT)>0){SWL_TOG(SWL_GREEN);}
-
-if (SWL_Pushed(SWL_CTR)>0){SWL_TOG(SWL_YELLOW);}
-
-if (SWL_Pushed(SWL_UP)>0){SWL_OFF(SWL_ALL);}
-
-if(SWL_Pushed(SWL_DOWN)>0 ){SWL_OFF(SWL_ALL);}
-
-
-
-unsigned char led_Count=0;
- if (SWL_Pushed(SWL_LEFT) > 0)
- {
-   while (led_Count < 2)
-   {
-     SWL_TOG(SWL_RED);
-     led_Count++;
-   }
- }
- if (SWL_Pushed(SWL_RIGHT)>0)
- {
-   while (led_Count < 2)
-   {
-     SWL_TOG(SWL_GREEN);
-     led_Count++;
-   }
- }
- if (SWL_Pushed(SWL_CTR)>0)
-
-    { while (led_Count<2)
+    /* if (SWL_Pushed(SWL_LEFT) > 0)
      {
-       
-         SWL_TOG(SWL_YELLOW);led_Count++; 
-
-
+       SWL_ON(SWL_RED);
      }
-   }
+     else if (SWL_Pushed(SWL_LEFT) == 0)
+     {
+       SWL_OFF(SWL_RED);
+     }
 
-if (SWL_Pushed(SWL_UP)>0){SWL_OFF(SWL_ALL); led_Count=0;}
+     if (SWL_Pushed(SWL_RIGHT) > 0)
+     {
+       SWL_ON(SWL_GREEN);
+     }
+     else if (SWL_Pushed(SWL_RIGHT) == 0)
+     {
+       SWL_OFF(SWL_GREEN);
+     }
 
- if(SWL_Pushed(SWL_DOWN)>0 ){SWL_OFF(SWL_ALL);led_Count=0;}
+     if (SWL_Pushed(SWL_CTR) > 0)
+     {
+       SWL_ON(SWL_YELLOW);
+     }
+     else if (SWL_Pushed(SWL_CTR) == 0)
+     {
+       SWL_OFF(SWL_YELLOW);
+     } */
+    // tier 2
 
- }   
-}
+    /*
+        if (SWL_Pushed(SWL_LEFT) > 0 )
+        {
+          SWL_TOG(SWL_RED);
+        }
 
-/********************************************************************/
-// Functions
-/********************************************************************/
+        if (SWL_Pushed(SWL_RIGHT) > 0)
+        {
+          SWL_TOG(SWL_GREEN);
+        }
 
-/********************************************************************/
-// Interrupt Service Routines
-/********************************************************************/
+        if (SWL_Pushed(SWL_CTR) > 0)
+        {
+          SWL_TOG(SWL_YELLOW);
+        }
 
+        if (SWL_Pushed(SWL_UP) > 0)
+        {
+          SWL_OFF(SWL_ALL);
+        }
 
+        if (SWL_Pushed(SWL_DOWN) > 0)
+        {
+          SWL_OFF(SWL_ALL);
+        }*/
 
+    if (SWL_Pushed(SWL_LEFT) > 0 & GetCount() <= 1)
+    {
+
+      SWL_TOG(SWL_RED);
+    }
+    if (SWL_Pushed(SWL_LEFT) > 0 & GetCount() == 2)
+    {
+      if (PT1AD1 == 0b11000000 || PT1AD1 == 0b10100000)
+      {
+        SWL_TOG(SWL_RED);
+      }
+    }
+
+    if (SWL_Pushed(SWL_RIGHT) > 0 & GetCount() <= 2)
+    {
+      SWL_TOG(SWL_GREEN);
+    }
+    if (SWL_Pushed(SWL_RIGHT) > 0 & GetCount() == 2)
+    {
+      if (PT1AD1 == 0b11000000 || PT1AD1 == 0b01100000)
+      {
+        SWL_TOG(SWL_GREEN);
+      }
+    }
+    if (SWL_Pushed(SWL_CTR) > 0 & GetCount() <= 1)
+    {
+      SWL_TOG(SWL_YELLOW);
+    }
+    if (SWL_Pushed(SWL_RIGHT) > 0 & GetCount() == 2)
+    {
+      if (PT1AD1 == 0b01100000 || PT1AD1 == 0b10100000)
+      {
+        SWL_TOG(SWL_GREEN);
+      }
+    }
+    if (SWL_Pushed(SWL_UP) > 0)
+    {
+      SWL_OFF(SWL_ALL);
+    }
+
+    if (SWL_Pushed(SWL_DOWN) > 0)
+    {
+      SWL_OFF(SWL_ALL);
+    }
   }}
+    /********************************************************************/
+    // Functions
+    /********************************************************************/
+    int GetCount(void)
+    {
+      int total_LEDS;
+
+      if (PT1AD1 == 0b0000000)
+      {
+        total_LEDS = 0;
+      }
+
+      if (PT1AD1 == 0b10000000 || PT1AD1 == 0b01000000 || PT1AD1 == 0b00100000)
+      {
+        total_LEDS = 1;
+      }
+      else if (PT1AD1 == 0b11000000 || PT1AD1 == 0b01100000 || PT1AD1 == 0b10100000)
+      {
+        total_LEDS = 2;
+      }
+      else if (PT1AD1 == 0b11100000)
+      {
+        total_LEDS = 3;
+      }
+
+      return total_LEDS;
+    }
+    /********************************************************************/
+    // Interrupt Service Routines
+    /********************************************************************/
+ 
