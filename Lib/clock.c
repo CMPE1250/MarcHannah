6567af2cee88a7eb2b854d3ca024d8a8c3c690b0
MarcHannah-NAIT <149616632+MarcHannah-NAIT@users.noreply.github.com>
2024-02-28 11:48:56 -0700

    Add files via upload

--- Lib/clock.c
+++ Lib/clock.c
@@ -10,23 +10,21 @@
 
 void Clock_EnableOutput(ClockOutDiv)
 {
-    CLKSEL=0b10000000
-    PLLCTL = PLLCTL_PLLON_MASK | PLLCTL_AUTO_MASK;
+    PLLCTL=ClockOutDiv;
 }
 
 void Clock_Set8MHZ(void)
 {
-    SYNR = 1;
-    REFDV = 1;
-    CLKSEL_PSTP = 1;
-    PLLCTL = 0b11111111;
-    while (!CRGFLG_LOCK)
-        CLKSEL_PLLSEL = 1;
+    
 }
 void Clock_Set20MHZ(void)
 {
     SYNR = 4;
     REFDV = 3;
+    CLKSEL_PSTP = 1;
+    PLLCTL = 0b11111111;
+    while (!CRGFLG_LOCK)
+        CLKSEL_PLLSEL = 1;
 }
 void Clock_Set24MHZ(void) { SYNR = }
 
