cb9a90baf6cdadf93ee574445ee2fced033d900b
MarcHannah-NAIT <149616632+MarcHannah-NAIT@users.noreply.github.com>
2024-03-01 16:05:03 -0700

    Add files via upload

--- Lib/clock.c
+++ Lib/clock.c
@@ -6,45 +6,48 @@
 #include "clock.h"
 
 
-  DEF_BUS_CLOCK 8000000; 
+  
 
-void Clock_EnableOutput(ClockOutDiv)
+void Clock_EnableOutput(ClockOutDiv Clock_Mask)
 {
-    PLLCTL=ClockOutDiv;
+    PLLCTL=Clock_Mask;
 }
 
 void Clock_Set8MHZ(void)
 {
     CLKSEL_PLLSEL = 0; 
+   // DEF_BUS_CLOCK=8000000;
 }
-void Clock_Set20MHZ(void)
+void Clock_Set20MHZ()
 {
     SYNR = 4;
     REFDV = 3;
     CLKSEL_PSTP = 1;
     PLLCTL = 0b11111111;
-    while (!CRGFLG_LOCK)
+    while (!CRGFLG_LOCK){;}
         CLKSEL_PLLSEL = 1;
+       // DEF_BUS_CLOCK=20000000;
 }
-void Clock_Set24MHZ(void) {
-    SYNR = 3;
-    REFDV = 2;
+void Clock_Set24MHZ() {
+    SYNR = 2;
+    REFDV = 1;
     CLKSEL_PSTP = 1;
     PLLCTL = 0b11111111;
-    while (!CRGFLG_LOCK)
-        CLKSEL_PLLSEL = 1; }
-
-void Clock_Set40MHZ(void){
-    SYNR =5;
-    REFDV = 2;
+    while (!CRGFLG_LOCK){;}
+        CLKSEL_PLLSEL = 1;
+      //  DEF_BUS_CLOCK=24000000; 
+}
+void Clock_Set40MHZ(){
+    SYNR =4;
+    REFDV = 1;
     CLKSEL_PSTP = 1;
     PLLCTL = 0b11111111;
-    while (!CRGFLG_LOCK)
+    while (!CRGFLG_LOCK){;}
         CLKSEL_PLLSEL = 1;
     
-    DEF_BUS_CLOCK
-    } // Set clock to 40MHZ using PLL
+   // DEF_BUS_CLOCK=40000000;
+    } 
 
-unsigned long Clock_GetBusSpeed(void)
+unsigned long Clock_GetBusSpeed()
 {long bSpeed=DEF_BUS_CLOCK;
 return bSpeed;}
