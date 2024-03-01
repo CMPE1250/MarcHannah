62c0840eca4040b90f3374e869db9989e918a621
MarcHannah-NAIT <149616632+MarcHannah-NAIT@users.noreply.github.com>
2024-03-01 16:06:33 -0700

    Add files via upload

--- ICAs/ICA7t/Sources/main.c
+++ ICAs/ICA7t/Sources/main.c
@@ -3,12 +3,10 @@
 // Processor:     MC9S12XDP512
 // Bus Speed:     MHz
 // Author:        This B. You
-// Details:       A more detailed explanation of the program is entered here               
+// Details:       A more detailed explanation of the program is entered here
 // Date:          Date Created
 // Revision History :
 //  each revision will have a date + desc. of changes
-
-
 
 /********************************************************************/
 // Library includes
@@ -17,13 +15,12 @@
 #include "derivative.h" /* derivative-specific definitions */
 #include "../../Lib/SWL_LED.h"
 
-//Other system includes or your includes go here
-//#include <stdlib.h>
-//#include <stdio.h>
-
+// Other system includes or your includes go here
+// #include <stdlib.h>
+// #include <stdio.h>
 
 /********************************************************************/
-//Defines
+// Defines
 /********************************************************************/
 
 /********************************************************************/
@@ -43,39 +40,45 @@
 /********************************************************************/
 void main(void)
 {
-  //Any main local variables must be declared here
+  // Any main local variables must be declared here
 
   // main entry point
   _DISABLE_COP();
-  //EnableInterrupts;
-  
-/********************************************************************/
-  // one-time initializations
-/********************************************************************/
- SWL_Init ();
+  // EnableInterrupts;
 
-/********************************************************************/
+  /********************************************************************/
+  // one-time initializations
+  /********************************************************************/
+  SWL_Init();
+
+  /********************************************************************/
   // main program loop
-/********************************************************************/
+  /********************************************************************/
 
   for (;;)
-  {  
-   countUp(5000);
+  {
+    countUp(25000);
     SWL_ON(SWL_RED);
-   countUp(5000);
-   SWL_OFF(SWL_RED);
+    countUp(25000);
+    SWL_OFF(SWL_RED);
 
-
-   if SWL_Pushed{SWL_LEFT}{Clock_Set8MHZ();}
-   if SWL_Pushed(SWL_CTR){Clock_Set20MHZ();}
-   
-
-
-
-    
-    
-    
-  }                   
+    if (SWL_Pushed(SWL_UP) > 0)
+    {
+      Clock_Set8MHZ();
+    }
+    if (SWL_Pushed(SWL_LEFT) > 0)
+    {
+      Clock_Set20MHZ();
+    }
+    if (SWL_Pushed(SWL_RIGHT) > 0)
+    {
+      Clock_Set24MHZ();
+    }
+    if (SWL_Pushed(SWL_DOWN) > 0)
+    {
+      Clock_Set40MHZ();
+    }
+  }
 }
 
 /********************************************************************/
