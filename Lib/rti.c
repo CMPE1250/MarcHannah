a32c7df451a41c20cbb3f2a9adbc5d270e08521c
MarcHannah-NAIT <149616632+MarcHannah-NAIT@users.noreply.github.com>
2024-03-08 14:13:48 -0700

    Add files via upload

--- Lib/rti.c
+++ Lib/rti.c
@@ -9,56 +9,24 @@ extern volatile unsigned long rtiMasterCount;
 void RTI_Init(void) { RTICTL = 0b11000111; }
 
 void RTI_Delay_ms(unsigned int ms)
-{   
-    
+{
+
     unsigned int count = ms;
-    if (ms == 8)
+
+    RTICTL = 0;
+
+    CRGFLG = CRGFLG_RTIF_MASK;
+
+    RTICTL = 0b10001111;
+
+    while (count > 0)
     {
-        RTICTL = 0b10111100;
-    }
-
-    if (ms == 12)
-    {
-        RTICTL = 0b11001001;
-    }
-
-
-
-
-    if (ms == 10)
-    {   
-        RTICTL = 0;
-
-        CRGFLG = CRGFLG_RTIF_MASK;
-
-        RTICTL = 0b10001111;
-
-       
-
-        while (count > 0)
+        if (CRGFLG_RTIF) // RTI period over?
         {
-            if (CRGFLG_RTIF) // RTI period over?
-            {
-                CRGFLG = CRGFLG_RTIF_MASK;
-            }
-            count--;
+            CRGFLG = CRGFLG_RTIF_MASK;
         }
-
-
-
+        count--;
     }
 
-
-
-
-
-
-    if (ms == 1)
-    {
-        RTICTL = 0b10001111;
-    }
-    if (ms == 9)
-    {
-        RTICTL = 0b10111110;
-    }
+    RTICTL = 0;
 }
