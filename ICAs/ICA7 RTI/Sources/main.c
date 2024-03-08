5bd33a449ecf7a51abe6f78862b3709410f77fd8
MarcHannah-NAIT <149616632+MarcHannah-NAIT@users.noreply.github.com>
2024-03-08 16:12:18 -0700

    Add files via upload

--- ICAs/ICA7 RTI/Sources/main.c
+++ ICAs/ICA7 RTI/Sources/main.c
@@ -1,13 +1,12 @@
 #include <hidef.h>      /* common defines and macros */
 #include "derivative.h" /* derivative-specific definitions */
 #include "../../Lib/SWL_LED.h"
-#include"../../Lib/rti.h"
+#include "../../Lib/rti.h"
 
 void main(void)
 {
   // main entry point
   _DISABLE_COP();
-  EnableInterrupts;
 
   SWL_Init();
   RTI_Init();
@@ -16,48 +15,46 @@ void main(void)
     if (SWL_Pushed(SWL_ANY) == 0)
     {
       RTI_Delay_ms(10);
+      SWL_TOG(SWL_RED);
     }
 
-    if (SWL_Pushed(SWL_UP) > 0)
+     if (SWL_Pushed(SWL_UP) > 0)
     {
       RTI_Delay_ms(8);
+      SWL_TOG(SWL_RED);
+
     }
 
     if (SWL_Pushed(SWL_DOWN) > 0)
     {
       RTI_Delay_ms(12);
+      SWL_TOG(SWL_RED);
     }
 
     if (((SWL_Pushed(SWL_UP)) > 0)
-      &&(SWL_Pushed(SWL_DOWN) > 0)) { RTI_Delay_ms(10); }
+      &&(SWL_Pushed(SWL_DOWN) > 0)) { RTI_Delay_ms(10);
+      SWL_TOG(SWL_RED); }
 
     /// tier 2
     if (SWL_Pushed(SWL_LEFT) > 0)
     {
+      SWL_ON(SWL_RED);
       RTI_Delay_ms(1);
-      if (CRGFLG_RTIF)
-      {
-        CRGFLG = CRGFLG_RTIF_MASK;
-        SWL_TOG(SWL_RED);
-      }
-      RTI_Delay_ms(9);
-      if (CRGFLG_RTIF)
-      {
-        CRGFLG = CRGFLG_RTIF_MASK;
-        SWL_TOG(SWL_RED);
-      }
+      SWL_OFF(SWL_RED);
+     RTI_Delay_ms(9);
+     
     }
     // tier 3
-    if (SWL_Pushed (SWL_RIGHT))
+    if (SWL_Pushed (SWL_RIGHT)>0)
     {
       SWL_ON(SWL_GREEN);
       SWL_OFF(SWL_RED);
     }
+  else {SWL_OFF(SWL_GREEN);}
+  
 
-    if (CRGFLG_RTIF) // RTI period over?
-    {
-      CRGFLG = CRGFLG_RTIF_MASK; // clear flag, VERY IMPORTANT
-      SWL_TOG(SWL_RED);
-    }
+
+
+
   }
 }
