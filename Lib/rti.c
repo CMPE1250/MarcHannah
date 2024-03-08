d9b4493b01e7f951a94422225603946ec17400fe
MarcHannah-NAIT <149616632+MarcHannah-NAIT@users.noreply.github.com>
2024-03-08 16:10:03 -0700

    Add files via upload

--- Lib/rti.c
+++ Lib/rti.c
@@ -1,32 +1,32 @@
-
-
 #include "derivative.h"
 #include <hidef.h>
 #include "rti.h"
 
 extern volatile unsigned long rtiMasterCount;
 
-void RTI_Init(void) { RTICTL = 0b11000111; }
+void RTI_Init(void)
+{
+    RTICTL=0;
+    CRGFLG = CRGFLG_RTIF_MASK;
+    RTICTL = 0b10001111;
+}
 
 void RTI_Delay_ms(unsigned int ms)
 {
 
-    unsigned int count = ms;
+    rtiMasterCount = 0;
 
-    RTICTL = 0;
+    RTI_Init();
 
-    CRGFLG = CRGFLG_RTIF_MASK;
-
-    RTICTL = 0b10001111;
-
-    while (count > 0)
+    while (rtiMasterCount < ms)
     {
         if (CRGFLG_RTIF) // RTI period over?
         {
             CRGFLG = CRGFLG_RTIF_MASK;
+            rtiMasterCount++;
         }
-        count--;
     }
 
-    RTICTL = 0;
+RTICTL=0;
+
 }
