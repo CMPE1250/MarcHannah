2ce770d2ab07db846c87a997539754fa9f90da04
MarcHannah-NAIT <149616632+MarcHannah-NAIT@users.noreply.github.com>
2024-03-19 21:16:31 -0600

    Add files via upload

--- ICA8 SCI/Sources/main.c
+++ ICA8 SCI/Sources/main.c
@@ -3,12 +3,10 @@
 // Processor:     MC9S12XDP512
 // Bus Speed:     MHz
 // Author:        This B. You
-// Details:       A more detailed explanation of the program is entered here               
+// Details:       A more detailed explanation of the program is entered here
 // Date:          Date Created
 // Revision History :
 //  each revision will have a date + desc. of changes
-
-
 
 /********************************************************************/
 // Library includes
@@ -17,14 +15,14 @@
 #include "derivative.h" /* derivative-specific definitions */
 #include "../../Lib/SWL_LED.h"
 #include "../../Lib/clock.h"
-#include "../../Lib/rti.h" 
-//Other system includes or your includes go here
+#include "../../Lib/rti.h"
+#include "../../Lib/misc.h"
+// Other system includes or your includes go here
 #include <stdlib.h>
-//#include <stdio.h>
-
+// #include <stdio.h>
 
 /********************************************************************/
-//Defines
+// Defines
 /********************************************************************/
 
 /********************************************************************/
@@ -34,75 +32,69 @@
 /********************************************************************/
 // Global Variables
 /********************************************************************/
- unsigned char data;
-/********************************************************************/
-// Constants
-/********************************************************************/
+char data;         // Variable to hold random char
+char receivedData; // Variable to hold received value
 
 /********************************************************************/
 // Main Entry
 /********************************************************************/
 void main(void)
 {
-  //Any main local variables must be declared here
+  // Any main local variables must be declared here
 
   // main entry point
   _DISABLE_COP();
-  //EnableInterrupts;
-  
-/********************************************************************/
+  // EnableInterrupts;
+
+  /********************************************************************/
   // one-time initializations
-/********************************************************************/
-SWL_Init();
-Clock_Set20MHZ(); 
-sci0_Init2();
-RTI_Init();
-/********************************************************************/
+  /********************************************************************/
+  
+    SWL_Init();
+    Clock_Set20MHZ();
+    sci0_Init2();
+    RTI_Init();
+
+  
+  /********************************************************************/
   // main program loop
-/********************************************************************/
+  /********************************************************************/
 
   for (;;)
   {
 
+    data = GetRandom(26) + 'A';
 
-  data=GetRandom() + 'A';
+    RTI_Delay_ms(50);
+    SWL_TOG(SWL_RED);
+    sci0_txByte(data);
 
-  RTI_Delay_ms(50);
-  SWL_TOG(SWL_RED);
-  sci0_txByte(data);
+    if (sci0_rxByte(&receivedData) == 1)
+
+    {
+      if (IsVowel(receivedData))
+      {
+        SWL_ON(SWL_GREEN);
+        SWL_OFF(SWL_YELLOW);
+      }
+      else
+      {
+
+        SWL_ON(SWL_YELLOW);
+        SWL_OFF(SWL_GREEN);
+      }
+    }
+    SWL_OFF(SWL_GREEN);
+    SWL_OFF(SWL_YELLOW);
 
 
 
-
-//sci0_rxByte(&data)
-  
- 
- 
-
-
-
-
- 
-
-
-
-
-
+  }
 }
+  /********************************************************************/
+  // Functions
+  /********************************************************************/
 
-
- 
- 
- 
- 
- 
-  }                   
-
-
-/********************************************************************/
-// Functions
-/********************************************************************/
-
-/********************************************************************/
-// Interrupt Service Routines
-/********************************************************************/
+  /********************************************************************/
+  // Interrupt Service Routines
+  /********************************************************************/
