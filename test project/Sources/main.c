762a04ddbd6459dad35b65ef0a8af69578fae849
MarcHannah-NAIT <149616632+MarcHannah-NAIT@users.noreply.github.com>
2024-04-03 09:29:01 -0600

    Add files via upload

--- test project/Sources/main.c
+++ test project/Sources/main.c
@@ -3,12 +3,10 @@
 // Processor:     MC9S12XDP512
 // Bus Speed:     MHz
 // Author:        This B. You
-// Details:       A more detailed explanation of the program is entered here               
+// Details:       A more detailed explanation of the program is entered here
 // Date:          Date Created
 // Revision History :
 //  each revision will have a date + desc. of changes
-
-
 
 /********************************************************************/
 // Library includes
@@ -16,7 +14,7 @@
 #include <hidef.h>      /* common defines and macros */
 #include "derivative.h" /* derivative-specific definitions */
 
-//Other system includes or your includes go here
+// Other system includes or your includes go here
 #include <stdlib.h>
 #include <stdio.h>
 #include "sci.h"
@@ -25,9 +23,8 @@
 #include "misc.h"
 #include "SWL_LED.h"
 
-
 /********************************************************************/
-//Defines
+// Defines
 /********************************************************************/
 
 /********************************************************************/
@@ -37,7 +34,13 @@ void GotoXY(int iCol, int iRow);
 /********************************************************************/
 // Global Variables
 /********************************************************************/
+char data;
 
+int count;
+int count2;
+
+
+ char buffer[10];
 /********************************************************************/
 // Constants
 /********************************************************************/
@@ -47,50 +50,102 @@ void GotoXY(int iCol, int iRow);
 /********************************************************************/
 void main(void)
 {
-  //Any main local variables must be declared here
+  // Any main local variables must be declared here
 
   // main entry point
   _DISABLE_COP();
-  //EnableInterrupts;
-  
-/********************************************************************/
+  // EnableInterrupts;
+
+  /********************************************************************/
   // one-time initializations
-/********************************************************************/
+  /********************************************************************/
   SWL_Init();
   Clock_Set20MHZ();
-  sci0_InitEnum(BAUD_19200);
+  sci0_InitEnum(BAUD_38400);
   RTI_Init();
 
-/********************************************************************/
+  /********************************************************************/
   // main program loop
-/********************************************************************/
+  /********************************************************************/
 
   for (;;)
   {
-      
-      char buffer[20]="string here";
-      RTI_Delay_ms(250);
-      //sci0_txStr("\x1B[5;10H");
-      
-      sci0_txStr(buffer);
-      RTI_Delay_ms(250);
-      sci0_ClearScreen();
 
-  }                   
+    data = '.';
+
+    if (SWL_Pushed(SWL_UP))
+    {
+      data = '!';
+    }
+
+    if (SWL_Pushed(SWL_DOWN))
+    {
+      data = '?';
+    }
+    if (SWL_Pushed(SWL_CTR))
+    {
+      data = '.';
+    }
+
+    RTI_Delay_ms(10);
+    SWL_TOG(SWL_RED);
+    sci0_txByte(data);
+
+    count++;
+    
+    
+    
+    
+      sprintf(buffer, "   %04d  ",count2 );
+
+
+   GotoXY(1,1);
+
+
+
+
+  
+   if (count2==0)
+   {
+    GotoXY(0,3);
+   }
+
+   if(count2<80){
+
+    if (count == 10)
+    {
+      sci0_txByte(data);
+       SWL_TOG(SWL_GREEN);
+       count2++;
+
+      count = 0;
+    }
+   
+
+   else if(count2==80)
+   {
+     sci0_txStr("\x1b[1K");
+
+     count2=0;
+   }
+
+
+
+
+  }
+  }
 }
 
 /********************************************************************/
 // Functions
 /********************************************************************/
 
+void GotoXY(int iCol, int iRow)
+{
 
-
-void GotoXY(int iCol, int iRow) {
-   
-   char location[20];
-    sprintf(location,"\x1B[%d;%dH", iRow, iCol);
-    sci0_txStr(location);
-    
+  char location[20];
+  sprintf(location, "\x1B[%d;%dH", iRow, iCol);
+  sci0_txStr(location);
 }
 
 /********************************************************************/
