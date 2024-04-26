07276578bc9cfd301d92731e626a4a722caddcf2
MarcHannah-NAIT <149616632+MarcHannah-NAIT@users.noreply.github.com>
2024-04-25 22:15:52 -0600

    Add files via upload

---
+++ EXAMPRACTICE3/Sources/main.c
@@ -0,0 +1,114 @@
+/********************************************************************/
+// HC12 Program:  YourProg - MiniExplanation
+// Processor:     MC9S12XDP512
+// Bus Speed:     MHz
+// Author:        This B. You
+// Details:       A more detailed explanation of the program is entered here
+// Date:          Date Created
+// Revision History :
+//  each revision will have a date + desc. of changes
+
+/********************************************************************/
+// Library includes
+/********************************************************************/
+#include <hidef.h>      /* common defines and macros */
+#include "derivative.h" /* derivative-specific definitions */
+
+// Other system includes or your includes go here
+#include <stdlib.h>
+#include "SWL_LED.h"
+#include "clock.h"
+#include "rti.h"
+#include "misc.h"
+#include "segs.h"
+#include <stdio.h>
+#include "lcd.h"
+/********************************************************************/
+// Defines
+/********************************************************************/
+
+/********************************************************************/
+// Local Prototypes
+/********************************************************************/
+
+/********************************************************************/
+// Global Variables
+/********************************************************************/
+
+int loopCount;
+int count;
+char countBuffer[20];
+int index=0;
+char binaryBuffer[18]="0000000000000000";
+
+/********************************************************************/
+// Constants
+/********************************************************************/
+
+/********************************************************************/
+// Main Entry
+/********************************************************************/
+void main(void)
+{
+  // Any main local variables must be declared here
+
+  // main entry point
+  _DISABLE_COP();
+  // EnableInterrupts;
+
+  /********************************************************************/
+  // one-time initializations
+  /********************************************************************/
+  Clock_Set20MHZ();
+  RTI_Init();
+  SWL_Init();
+  lcd_Init();
+  testInit();
+
+  /********************************************************************/
+  // main program loop
+  /********************************************************************/
+
+  for (;;)
+  {
+
+
+    
+     RTI_Delay_ms(10);
+     loopCount++;
+      if (loopCount % 10 == 0)
+      {
+         count += 1;
+      }
+    
+
+     sprintf(countBuffer,"Count %d",count);
+     lcd_AddrXY(0,0);
+     lcd_String(countBuffer);
+
+    if(SWL_Pushed(SWL_DOWN)||SWL_Pushed(SWL_UP))
+    {
+      SWL_TOG(SWL_GREEN);
+      if(binaryBuffer[index]==0)
+      {
+        binaryBuffer[index]+=1;
+      }
+      else{binaryBuffer[index]=0;}
+    }
+   
+   
+    lcd_AddrXY(2,3);
+    lcd_String(binaryBuffer);
+    //lcd_Ins(00000001);
+
+
+  }
+}
+
+/********************************************************************/
+// Functions
+/********************************************************************/
+
+/********************************************************************/
+// Interrupt Service Routines
+/********************************************************************/
